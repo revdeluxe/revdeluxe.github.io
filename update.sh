b5d932aa6dfe079f9da37ea091b7d6bef8fa2e94
revdeluxe <revchannel01@gmail.com>
2021-11-09 14:46:58 +0800

    Committed Using A ShellScript

--- update.sh
+++ update.sh
@@ -4,20 +4,6 @@ sec=2
 clear
 git add --all
 echo "ADDING STUFFS"
-        while [ $hour -ge 0 ]; do
-                 while [ $min -ge 0 ]; do
-                         while [ $sec -ge 0 ]; do
-                                 echo -ne "$hour:$min:$sec\033[0K\r"
-                                 let "sec=sec-1"
-                                 sleep 1
-                         done
-                         sec=59
-                         let "min=min-1"
-                 done
-                 min=59
-                 let "hour=hour-1"
-		clear
-		done
 git commit -m "Committed Using A ShellScript"
 echo "Committing"
         while [ $hour -ge 0 ]; do
